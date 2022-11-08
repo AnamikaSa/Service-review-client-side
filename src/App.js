9774a8932265902679725e85b5a7da4b7a0ee82d
AnamikaSa <amanika0061@gmail.com>
2022-11-08 14:43:10 +0600

    about us added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import router from './Routes/Routes';
 
 function App() {
   return (
-    <div  data-theme="cupcake">
+    <div  data-theme="light">
       <RouterProvider router={router}></RouterProvider>
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import banner from '../images/banner.jpg';
+import about from '../images/about.jpg';
 import './Home.css';
 const Home = () => {
-    return (
-        <div className='banner'>
-            <div className="d-grid gap-5 p-5 text-center text-white">
-        <br/>
+  return (
+    <div>
+    <div className='banner'>
+      <div className="d-grid gap-5 p-5 text-center text-white">
+        <br />
         <h1 className='text-4xl mt-28 font-bold'>
           Exploring the World
         </h1>
-        <br/><br/>
-        <p>Let's enjoy every moment of life while traveling. <br/>
+        <br /><br />
+        <p>Let's enjoy every moment of life while traveling. <br />
           If you are looking for a proper agency ,here you are!!</p>
       </div>
       <div class="btn-toolbar ml-96 pl-20 mt-5">
@@ -20,9 +21,25 @@ const Home = () => {
         <div className="btn-group">
           <button className="btn btn-outline-secondary rounded-pill text-white"> Book Now</button>
         </div>
-        </div>
-        </div>
-    );
+      </div>
+    </div>
+
+    {/* About part */}
+    <h1 className='text-4xl text-center font-bold mt-8 '>About Us</h1>
+    <div className='hero-content mt-5 flex-col lg:flex-row'>
+    
+      <p>Travel is the movement of people between distant geographical locations. <br/>Travel can be done by foot, bicycle, automobile, train, boat, bus, airplane, <br/>ship or other means, with or without luggage, and can be one way or <br/> round trip.<br/>The meaning of TOUR is a journey for business, pleasure, or<br/> education often involving a series of stops and ending at <br/>the starting point.
+       <br /><br/>Our services are like that.We try to give our best serviecs.Our expert <br></br> guides take you to the must-see destinations as well as the undiscovered <br/>places to enjoy wonderful tours in Rome in small groups or in <br/> private tours.</p>
+      
+        <img src={about} alt="" className='rounded shadow-2xl' style={{width:'500px'}}></img>
+    
+      
+    </div>
+
+
+    </div>
+
+  );
 };
 
 export default Home;
