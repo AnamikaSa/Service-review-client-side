7cb3c516f99b653013c0f5a12d781331b7144d96
AnamikaSa <amanika0061@gmail.com>
2022-11-08 10:55:06 +0600

    header and footer added

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,8 +1,18 @@
+import Home from "../components/Home";
+import Main from "../components/Layout/Main";
+
 const { createBrowserRouter } = require("react-router-dom");
 
 const router= createBrowserRouter([
     {
         path:'/',
+        element:<Main></Main>,
+        children:[
+            {
+                path:'/',
+                element:<Home></Home>
+            }
+        ]
     }
 ]);
 
