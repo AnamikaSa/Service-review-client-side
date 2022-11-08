d9cd7ad4c70c280ce13d70de72145e3bbb273473
AnamikaSa <amanika0061@gmail.com>
2022-11-08 11:47:31 +0600

    banner added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,26 @@
 import React from 'react';
-
+import banner from '../images/banner.jpg';
+import './Home.css';
 const Home = () => {
     return (
-        <div>
-            
+        <div className='banner'>
+            <div className="d-grid gap-5 p-5 text-center text-white">
+        <br/>
+        <h1 className='text-4xl mt-28 font-bold'>
+          Exploring the World
+        </h1>
+        <br/><br/>
+        <p>Let's enjoy every moment of life while traveling. <br/>
+          If you are looking for a proper agency ,here you are!!</p>
+      </div>
+      <div class="btn-toolbar ml-96 pl-20 mt-5">
+        <div className="btn-group me-4">
+          <button class="btn btn-primary rounded-pill"> Learn More</button>
+        </div>
+        <div className="btn-group">
+          <button className="btn btn-outline-secondary rounded-pill text-white"> Book Now</button>
+        </div>
+        </div>
         </div>
     );
 };
